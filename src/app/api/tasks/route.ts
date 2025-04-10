85b58f2588e3a358a1abe1a97a6c416ee6cd4279
André Brasil <andremiguelbrasil@gmail.com>
2025-04-10 10:55:13 +0100

    remove old code

--- src/app/api/tasks/route.ts
+++ src/app/api/tasks/route.ts
@@ -3,27 +3,6 @@ import postgres from 'postgres';
 
 const sql = postgres(process.env.POSTGRES_CONN_STRING!)
 
-const tasks: Task[] = [
-        {
-            createdBy: "John Doe",
-            title: "Implement a new endpoint in the ODS API",
-            summary: "This tasks involves writing some typespec and some database querying!",
-            tags: ["C#", ".NET", "SQL"],
-        },
-        {
-            createdBy: "Jane Doe",
-            title: "Sort out Jane's stuff so she can start development",
-            summary: "Involves setting up local environemnt on the teriyaki project.",
-            tags: ["Next.js", "Docker", "Windows"],
-        },
-        {
-            createdBy: "Rob Smith",
-            title: "Display a warning message when viewing performance/valuation history of a new portfolio/account",
-            summary: "You're welcome to pair up with me on this task which involves some front-end work.",
-            tags: ["Next.js", "TypeScript"],
-        },
-    ]
-
 export async function GET() {
     const tasks = await sql<Task[]>`
         SELECT title, summary, created_by, tags FROM task
