db2aed78d9a39802b094c437cc76a16c0fd614c3
André Brasil <andremiguelbrasil@gmail.com>
2025-04-11 15:28:55 +0100

    set title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,11 @@ import { TaskCard } from "@/components/task-card"
 import * as actions from '@/actions'
 import { CreateTaskForm } from "@/components/tasks/create-task-form";
 import { Task } from "@/lib/dtos";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "dashboard"
+}
 
 export default async function Page() {
   const tasks: Task[] = await actions.getTasks()
