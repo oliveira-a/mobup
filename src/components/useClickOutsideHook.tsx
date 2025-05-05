7f2fd2d920997a088f1189bad3c28dc2ce46ebaa
André Brasil <andremiguelbrasil@gmail.com>
2025-05-05 10:34:39 +0100

    fix issues

--- src/components/useClickOutsideHook.tsx
+++ src/components/useClickOutsideHook.tsx
@@ -12,28 +12,8 @@ export const useClickOutside = (
 
   useEffect(() => {
     document.addEventListener('click', handleClick)
-import { RefObject, useEffect } from 'react'
-
-export const useClickOutside = (
-  ref: RefObject<HTMLElement | undefined>,
-  callback: () => void
-) => {
-  const handleClick = (event: MouseEvent) => {
-    if (ref.current && !ref.current.contains(event.target as HTMLElement)) {
-      callback()
-    }
-  }
-
-  useEffect(() => {
-    document.addEventListener('click', handleClick)
     return () => {
       document.removeEventListener('click', handleClick)
     }
   })
 }
-    return () => {
-      document.removeEventListener('click', handleClick)
-    }
-  })
-}
-
